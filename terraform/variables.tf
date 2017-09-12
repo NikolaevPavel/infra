9de9c447a8f2a9d90d53f2382f72e975ceb314f4
NikolaevPavel <pavel.nikolaev@aton.ru>
2017-09-12 15:51:55 +0300

    terraform

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,30 +7,12 @@ variable region {
   default     = "europe-west1"
 }
 
-variable public_key_path {
-  description = "Path to the ssh key for ssh access"
-}
-
 variable disk_image {
   description = "diskimage"
 }
 
-variable private_key_path {
-  description = "variable project {
-  description = "project ID"
-}
-
-variable region {
-  description = "region"
-  default     = "europe-west1"
-}
-
 variable public_key_path {
   description = "path to the public key used for ssh access"
-}
-
-variable disk_image {
-  description = "disk image"
 }
 
 variable private_key_path {
